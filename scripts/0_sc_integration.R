7aac3d55a64207b3fd2fe555777c1de43bbb6ccb
Luiz Felipe Piochi <luizpiochi@outlook.com>
2023-10-19 19:17:08 +0100

    Update 0_sc_integration.R
    Update according to reviewer's comments.

--- scripts/0_sc_integration.R
+++ scripts/0_sc_integration.R
@@ -17,14 +17,8 @@ introductory vignette.
 # anno represents the genes in common between all datasets
 anno <- read.csv("hGENES.csv", header = T)
 
-# KAGOHARA_SCC1
-kag <- read.csv("Kagohara/SCC1Matrix.csv")
-kag <- kag[kag[,1] %in% anno[, 1], ]
-kag <- kag[!duplicated(kag[, 1]),]
-rownames(kag) <- kag[, 1]
-kag <- kag[ , grepl( "PBS" , colnames(kag))]
-
-kagohara1 <- CreateSeuratObject(counts = kag, min.cells = 0, assay = "RNA", project = "Kagohara", min.features = 100)
+mt_cutoff <- 10
+lower_cutoff <- 1000
 
 # KAGOHARA_SCC25
 kag <- read.csv("Kagohara/SCC25Matrix.csv")
@@ -33,56 +27,61 @@ kag <- kag[!duplicated(kag[, 1]),]
 rownames(kag) <- kag[, 1]
 kag <- kag[ , grepl( "PBS" , colnames(kag))]
 
-kagohara2 <- CreateSeuratObject(counts = kag, min.cells = 0, assay = "RNA", project = "Kagohara", min.features = 100)
+kagohara2 <- CreateSeuratObject(counts = kag, min.cells = 0, assay = "RNA", project = "Kagohara", min.features = lower_cutoff)
+kagohara2[["percent.mt"]] <- PercentageFeatureSet(kagohara2, pattern = "^MT-")
+kagohara2 <- subset(kagohara2, percent.mt < mt_cutoff)
 
 # BENDAVID
 ben <- read.csv("BenDavid/GSE114461.csv")
 ben <- ben[rownames(ben) %in% anno[, 1], ]
 ben <- ben[ , grepl( "t0" , colnames(ben))]
 
-bendavid <- CreateSeuratObject(counts = ben, project = "BenDavid" , min.cells = 0, assay = "RNA", min.features = 100)
+bendavid <- CreateSeuratObject(counts = ben, project = "BenDavid" , min.cells = 0, assay = "RNA", min.features = lower_cutoff)
+bendavid[["percent.mt"]] <- PercentageFeatureSet(bendavid, pattern = "^MT-")
+bendavid <- subset(bendavid, percent.mt < mt_cutoff)
 
 # MCFARLAND
 mcfarland <- Read10X("McFarland/")
 mcfarland <- mcfarland[rownames(mcfarland) %in% anno[, 1], ]
-mcfarland <- CreateSeuratObject(counts = mcfarland, min.cells = 0, assay = "RNA", project = "mcfar", min.features = 100)
+
+mcfarland <- CreateSeuratObject(counts = mcfarland, min.cells = 0, assay = "RNA", project = "mcfar", min.features = lower_cutoff)
+mcfarland[["percent.mt"]] <- PercentageFeatureSet(mcfarland, pattern = "^MT-")
+mcfarland <- subset(mcfarland, percent.mt < mt_cutoff)
 
 # SCHNEPP
 schnepp <- read.csv("Schnepp/GSE140440.csv", header = T)
-schnepp <- separate(schnepp, col = 'X0', into = c('bla', 'genes'), sep = '\\|')
-schnepp <- schnepp[, -1]
+schnepp <- separate(schnepp, X0, c(NA, 'genes'), sep = "\\|")
 schnepp <- schnepp[!duplicated(schnepp[, 'genes']),]
 rownames(schnepp) <- schnepp[, 1]
-anno <- read.csv("hGENES.csv", header = T)
 schnepp <- schnepp[, -1]
 schnepp <- schnepp[rownames(schnepp) %in% anno[, 1], ]
 
-schnepp <- CreateSeuratObject(counts = schnepp, min.cells = 0, assay = "RNA", project = "Schnepp", min.features = 100)
+schnepp <- CreateSeuratObject(counts = schnepp, min.cells = 0, assay = "RNA", project = "Schnepp", min.features = lower_cutoff)
+schnepp[["percent.mt"]] <- PercentageFeatureSet(schnepp, pattern = "^MT-")
+schnepp <- subset(schnepp, percent.mt < mt_cutoff)
 
 # SRIRAMKUMAR
 srilanka <- Read10X("Sriramkumar/")
 srilanka <- srilanka[rownames(srilanka) %in% anno[, 1], ]
 colnames(srilanka) <- paste0(colnames(srilanka), "_", "OVCAR3")
 
-sriramkumar <- CreateSeuratObject(counts = srilanka, min.cells = 0, assay = "RNA", project = "Sriramkumar", min.features = 100)
+sriramkumar <- CreateSeuratObject(counts = srilanka, min.cells = 0, assay = "RNA", project = "Sriramkumar", min.features = lower_cutoff)
+sriramkumar[["percent.mt"]] <- PercentageFeatureSet(sriramkumar, pattern = "^MT-")
+sriramkumar <- subset(sriramkumar, percent.mt < mt_cutoff)
 
-# Integrate the datasets
-data_ais <- c(kagohara1, kagohara2, bendavid, mcfarland, schnepp, sriramkumar)
-data_ais <- lapply(X = data_ais, FUN = function(x) {
-  x <- NormalizeData(x)
-  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)})
+merged_dataset <- merge(kagohara2, y = c(bendavid, mcfarland, schnepp, sriramkumar),
+                        add.cell.ids = c("kagohara2", "ben-david", "mcfarland", "schnepp", "sriramkumar"))
 
-features <- SelectIntegrationFeatures(object.list = data_ais)
-anchors <- FindIntegrationAnchors(object.list = data_ais, anchor.features = features)
-data_s <- IntegrateData(anchorset = anchors)
-
-data_s <- subset(data_s, subset = nFeature_RNA > 100 & nFeature_RNA < 8000)
 s.genes <- cc.genes$s.genes
 g2m.genes <- cc.genes$g2m.genes
-data_s <- CellCycleScoring(data_s, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-data_s <- ScaleData(data_s, vars.to.regress = c("S.Score", "G2M.Score"))
 
-datas <- as.data.frame(t(data_s@assays$integrated@scale.data))
+data_s <- subset(merged_dataset, subset = nFeature_RNA > 1000 & nFeature_RNA < 7500)
+data_s <- CellCycleScoring(data_s, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+data_s <- NormalizeData(data_s)
+data_s <- FindVariableFeatures(data_s, selection.method = "vst", nfeatures = 2000)
+data_s <- ScaleData(data_s)
+
+datas <- as.data.frame(t(data_s@assays$RNA@scale.data))
 
 write.csv(datas, paste(DEFAULT_LOCATION, "data/single-cell/integrated_sc.csv", sep = ""), row.names = T)
 
